b842e16e842d61aef78f4bebc713753512b855e5
sagar <sagarjain2010.sj@gmail.com>
2020-03-01 23:02:51 +0530

    added angular project inside spring boot service

---
+++ wizfrontend/src/main/java/com/task/wizfrontend/WizfrontendApplication.java
@@ -0,0 +1,13 @@
+package com.task.wizfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizfrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WizfrontendApplication.class, args);
+	}
+
+}
---
+++ wizfrontend/src/test/java/com/task/wizfrontend/WizfrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.wizfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizfrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
